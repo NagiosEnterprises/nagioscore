a319c2daa68ca3fa350ea7e2261db3d66081ceff
Ethan Galstad <egalstad@nagios.org>
2002-11-25 02:59:26 +0000

    Prep for 1.0 release
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@174 5f96b256-904b-4d8d-8c98-d829582c6739

--- common/common.h
+++ common/common.h
@@ -22,8 +22,8 @@
  ************************************************************************/
 
 
-#define PROGRAM_VERSION "1.0b6"
-#define PROGRAM_MODIFICATION_DATE "09-15-2002"
+#define PROGRAM_VERSION "1.0"
+#define PROGRAM_MODIFICATION_DATE "11-24-2002"
 
 
 /***************************** COMMANDS *********************************/

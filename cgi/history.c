9c0b8873e01327bc93e56c39091d37d879be8399
Ethan Galstad <egalstad@nagios.org>
2003-05-06 03:33:26 +0000

    Bug fix in service history display
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@308 5f96b256-904b-4d8d-8c98-d829582c6739

--- cgi/history.c
+++ cgi/history.c
@@ -3,7 +3,7 @@
  * HISTORY.C - Nagios History CGI
  *
  * Copyright (c) 1999-2003 Ethan Galstad (nagios@nagios.org)
- * Last Modified: 02-26-2003
+ * Last Modified: 05-05-2003
  *
  * This CGI program will display the history for the specified host.
  * If no host is specified, the history for all hosts will be displayed.
@@ -839,6 +839,15 @@ void get_history(void){
 
 				if(strstr(temp_buffer,match1))
 					display_line=TRUE;
+
+				if(display_line==TRUE){
+					if(history_options==HISTORY_ALL || history_options==HISTORY_SERVICE_ALL)
+						display_line=TRUE;
+					else if(history_options & history_detail_type)
+						display_line=TRUE;
+					else 
+						display_line=FALSE;
+			                }
 
 				/* check alert state type */
 				if(display_line==TRUE && history_type==SERVICE_HISTORY){

a07027d69a3f1a8de26f02f200ad467fb1d0c347
Ethan Galstad <egalstad@nagios.org>
2004-01-21 05:16:56 +0000

    minor buglet fix
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/branches/nagios-1-x-bugfixes@445 5f96b256-904b-4d8d-8c98-d829582c6739

--- cgi/extinfo.c
+++ cgi/extinfo.c
@@ -2,8 +2,8 @@
  *
  * EXTINFO.C -  Nagios Extended Information CGI
  *
- * Copyright (c) 1999-2002 Ethan Galstad (nagios@nagios.org)
- * Last Modified: 08-27-2002
+ * Copyright (c) 1999-2004 Ethan Galstad (nagios@nagios.org)
+ * Last Modified: 01-20-2004
  *
  * License:
  * 
@@ -1858,7 +1858,7 @@ void display_comments(int type){
 	        }
 
 
-	printf("<A NAME=COMMENTS></A>\n");
+	printf("<A NAME=comment></A>\n");
 	printf("<DIV CLASS='commentTitle'>%s Comments</DIV>\n",(type==HOST_COMMENT)?"Host":"Service");
 	printf("<TABLE BORDER=0>\n");
 

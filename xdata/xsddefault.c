3899bc8f911872002ecb48561748e2fd3f08c1c6
Ethan Galstad <egalstad@nagios.org>
2005-11-26 07:05:36 +0000

    Sanity checks for full partitions during status file update
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@589 5f96b256-904b-4d8d-8c98-d829582c6739

--- xdata/xsddefault.c
+++ xdata/xsddefault.c
@@ -3,7 +3,7 @@
  * XSDDEFAULT.C - Default external status data input routines for Nagios
  *
  * Copyright (c) 2000-2005 Ethan Galstad (nagios@nagios.org)
- * Last Modified:   05-25-2005
+ * Last Modified:   11-25-2005
  *
  * License:
  *
@@ -289,6 +289,7 @@ int xsddefault_cleanup_status_data(char *config_file, int delete_status_data){
 
 /* write all status data to file */
 int xsddefault_save_status_data(void){
+	char temp_buffer[MAX_INPUT_BUFFER];
 	host *temp_host;
 	service *temp_service;
 	time_t current_time;
@@ -298,12 +299,26 @@ int xsddefault_save_status_data(void){
 	/* open a safe temp file for output */
 	snprintf(xsddefault_aggregate_temp_file,sizeof(xsddefault_aggregate_temp_file)-1,"%sXXXXXX",xsddefault_temp_file);
 	xsddefault_aggregate_temp_file[sizeof(xsddefault_aggregate_temp_file)-1]='\x0';
-	if((fd=mkstemp(xsddefault_aggregate_temp_file))==-1)
+	if((fd=mkstemp(xsddefault_aggregate_temp_file))==-1){
+
+		/* log an error */
+		snprintf(temp_buffer,sizeof(temp_buffer),"Error: Unable to create temp file for writing status data!\n");
+		temp_buffer[sizeof(temp_buffer)-1]='\x0';
+		write_to_logs_and_console(temp_buffer,NSLOG_RUNTIME_ERROR,TRUE);
+
 		return ERROR;
+	        }
 	fp=fdopen(fd,"w");
 	if(fp==NULL){
+
 		close(fd);
 		unlink(xsddefault_aggregate_temp_file);
+
+		/* log an error */
+		snprintf(temp_buffer,sizeof(temp_buffer),"Error: Unable to open temp file '%s' for writing status data!\n",xsddefault_aggregate_temp_file);
+		temp_buffer[sizeof(temp_buffer)-1]='\x0';
+		write_to_logs_and_console(temp_buffer,NSLOG_RUNTIME_ERROR,TRUE);
+
 		return ERROR;
 	        }
 
@@ -468,8 +483,15 @@ int xsddefault_save_status_data(void){
 	fclose(fp);
 
 	/* move the temp file to the status log (overwrite the old status log) */
-	if(my_rename(xsddefault_aggregate_temp_file,xsddefault_status_log))
+	if(my_rename(xsddefault_aggregate_temp_file,xsddefault_status_log)){
+
+		/* log an error */
+		snprintf(temp_buffer,sizeof(temp_buffer),"Error: Unable to update status data file '%s'!\n",xsddefault_status_log);
+		temp_buffer[sizeof(temp_buffer)-1]='\x0';
+		write_to_logs_and_console(temp_buffer,NSLOG_RUNTIME_ERROR,TRUE);
+
 		return ERROR;
+	        }
 
 	return OK;
         }

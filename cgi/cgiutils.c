6385311bf3de2aeb2a52e45056376b0ad8e5c7fa
Ethan Galstad <egalstad@nagios.org>
2007-03-02 23:55:42 +0000

    Doc updates
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@860 5f96b256-904b-4d8d-8c98-d829582c6739

--- cgi/cgiutils.c
+++ cgi/cgiutils.c
@@ -2,8 +2,8 @@
  *
  * CGIUTILS.C - Common utilities for Nagios CGIs
  * 
- * Copyright (c) 1999-2006 Ethan Galstad (nagios@nagios.org)
- * Last Modified: 11-12-2006
+ * Copyright (c) 1999-2007 Ethan Galstad (nagios@nagios.org)
+ * Last Modified: 03-02-2007
  *
  * License:
  *
@@ -1846,14 +1846,14 @@ void include_ssi_files(char *cgi_name, int type){
 		cgi_ssi_file[x]=tolower(cgi_ssi_file[x]);
 
 	if(type==SSI_HEADER){
-		printf("\n<!-- Produced by Nagios (http://www.nagios.org).  Copyright (c) 1999-2003 Ethan Galstad. -->\n");
+		printf("\n<!-- Produced by Nagios (http://www.nagios.org).  Copyright (c) 1999-2007 Ethan Galstad. -->\n");
 		include_ssi_file(common_ssi_file);
 		include_ssi_file(cgi_ssi_file);
 	        }
 	else{
 		include_ssi_file(cgi_ssi_file);
 		include_ssi_file(common_ssi_file);
-		printf("\n<!-- Produced by Nagios (http://www.nagios.org).  Copyright (c) 1999-2003 Ethan Galstad. -->\n");
+		printf("\n<!-- Produced by Nagios (http://www.nagios.org).  Copyright (c) 1999-2007 Ethan Galstad. -->\n");
 	        }
 
 	return;

98b88205c11cd7885d7031f6c63d9b915d466104
Ethan Galstad <egalstad@nagios.org>
2009-07-31 15:50:58 +0000

    Changed sourceforge logo to use local instead of remote copy
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@1326 5f96b256-904b-4d8d-8c98-d829582c6739

--- html/main.php
+++ html/main.php
@@ -24,9 +24,10 @@ include_once(dirname(__FILE__).'/includes/utils.inc.php');
 </div>
 
 <div id="currentversioninfo">
+<div class="product">Nagios Core</div>
 <div class="version">Version 3.1.2</div>
 <div class="releasedate">June 23, 2009</div>
-<div class="whatsnew"><a href="docs/whatsnew.html">Read what's new in Nagios 3</a></div>
+<div class="whatsnew"><a href="docs/whatsnew.html">Read what's new in Nagios Core 3</a></div>
 </div>
 
 
@@ -66,7 +67,7 @@ Nagios is licensed under the GNU General Public License and is provided AS IS wi
 
 <a href="http://www.nagios.org/" target="_blank"><img src="images/weblogo1.png" width="102" height="47" border="0" style="padding: 0 40px 0 40px;" title="Nagios.org"></a>
 
-<a href="http://sourceforge.net/projects/nagios"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=26589&amp;type=2" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a>
+<a href="http://sourceforge.net/projects/nagios"><img src="images/sflogo.png" width="88" height="31" border="0" alt="SourceForge.net Logo" /></a>
 </div>
 </div> 
 

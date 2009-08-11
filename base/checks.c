ebf40e8848d597674968b3f5df31696ea14d41c5
Ethan Galstad <egalstad@nagios.org>
2009-08-11 16:56:39 +0000

    Removed some hanging debug code
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@1341 5f96b256-904b-4d8d-8c98-d829582c6739

--- base/checks.c
+++ base/checks.c
@@ -279,10 +279,12 @@ int run_scheduled_service_check(service *svc, int check_options, double latency)
 			/* make sure we rescheduled the next service check at a valid time */
 			get_next_valid_time(preferred_time,&next_valid_time,svc->check_period_ptr);
 
+			/*
 			logit(NSLOG_RUNTIME_WARNING,TRUE,"Warning: Service '%s' on host '%s' timeperiod check failed...\n",svc->description,svc->host_name);
 			logit(NSLOG_RUNTIME_WARNING,TRUE,"Current time: %s",ctime(&current_time));
 			logit(NSLOG_RUNTIME_WARNING,TRUE,"Preferred time: %s",ctime(&preferred_time));
 			logit(NSLOG_RUNTIME_WARNING,TRUE,"Next valid time: %s",ctime(&next_valid_time));
+			*/
 
 			/* the service could not be rescheduled properly - set the next check time for next week */
 			/*if(time_is_valid==FALSE && next_valid_time==preferred_time){*/

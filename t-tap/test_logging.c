1355ccbb0a8592558373c9adc9885f2ee60e4953
Ton Voon <tonvoon@mac.com>
2009-09-11 23:21:58 +0000

    Fixed test which was not timezone aware
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@1356 5f96b256-904b-4d8d-8c98-d829582c6739

--- t-tap/test_logging.c
+++ t-tap/test_logging.c
@@ -74,10 +74,16 @@ main (int argc, char **argv)
 {
 	time_t rotation_time;
 	struct stat stat_info, stat_new;
+	char *log_filename_localtime = NULL;
+	char *temp_command = NULL;
+	struct tm *t;
 
 	plan_tests(14);
 
 	rotation_time = (time_t)1242949698;
+	t=localtime(&rotation_time);
+
+	asprintf(&log_filename_localtime,"var/nagios-%02d-%02d-%d-%02d.log", t->tm_mon+1,t->tm_mday,t->tm_year+1900,t->tm_hour);
 
 	log_rotation_method=5;
 	ok(rotate_log_file(rotation_time) == ERROR, "Got error for a bad log_rotation_method");
@@ -85,7 +91,7 @@ main (int argc, char **argv)
 	log_file="renamefailure";
 	log_rotation_method=LOG_ROTATION_HOURLY;
 	ok(rotate_log_file(rotation_time) == ERROR, "Got an error with rename");
-	ok( strcmp(saved_dest, "var/nagios-05-22-2009-00.log")==0, "Got an hourly rotation" );
+	ok( strcmp(saved_dest, log_filename_localtime)==0, "Got an hourly rotation" );
 
 	log_file="var/nagios.log";
 	log_rotation_method=LOG_ROTATION_HOURLY;
@@ -93,13 +99,15 @@ main (int argc, char **argv)
 	ok(rotate_log_file(rotation_time) == OK, "Log rotation should work happily");
 
 	ok( system("diff var/nagios.log var/nagios.log.expected > /dev/null")==0, "Got correct contents of nagios.log");
-	ok( system("diff var/nagios.log.dummy var/nagios-05-22-2009-00.log")==0, "nagios log archived correctly" );
 
-	unlink("var/nagios-05-22-2009-00.log");
+	asprintf(&temp_command, "diff var/nagios.log.dummy %s", log_filename_localtime);
+	ok( system(temp_command)==0, "nagios log archived correctly" );
+
+	unlink(log_filename_localtime);
 	ok( system("chmod 777 var/nagios.log")==0, "Changed mode of nagios.log" );
 	ok( stat("var/nagios.log", &stat_info) == 0, "Got stat info for log file" );
 	ok( rotate_log_file(rotation_time) == OK, "Log rotate to check if mode is retained" );
-	ok( stat("var/nagios-05-22-2009-00.log", &stat_new) == 0, "Got new stat info for archived log file" );
+	ok( stat(log_filename_localtime, &stat_new) == 0, "Got new stat info for archived log file" );
 	ok( stat_info.st_mode == stat_new.st_mode, "Mode for archived file same as original log file" );
 
 	ok( stat("var/nagios.log", &stat_new) == 0, "Got new stat info for new log file" );

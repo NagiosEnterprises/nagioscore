40ffd1c9050242fd09085c937af85ca71af828a1
Ethan Galstad <egalstad@nagios.org>
2010-08-05 02:19:54 +0000

    Fix for services inheriting notification period from hosts if not defined (Gordon Messmer)
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@1415 5f96b256-904b-4d8d-8c98-d829582c6739

--- base/notifications.c
+++ base/notifications.c
@@ -4,7 +4,7 @@
  *
  * Copyright (c) 2009-2010 Nagios Core Development Team and Community Contributors
  * Copyright (c) 1999-2008 Ethan Galstad (egalstad@nagios.org)
- * Last Modified: 06-02-2010
+ * Last Modified: 08-04-2010
  *
  * License:
  *
@@ -295,6 +295,7 @@ int service_notification(service *svc, int type, char *not_author, char *not_dat
 /* checks the viability of sending out a service alert (top level filters) */
 int check_service_notification_viability(service *svc, int type, int options){
 	host *temp_host;
+	timeperiod *temp_period;
 	time_t current_time;
 	time_t timeperiod_start;
 	time_t first_problem_time;
@@ -326,8 +327,14 @@ int check_service_notification_viability(service *svc, int type, int options){
 		return ERROR;
 	        }
 
+	/* if the service has no notification period, inherit one from the host */
+	temp_period=svc->notification_period_ptr;
+	if(temp_period==NULL){
+		temp_period=svc->host_ptr->notification_period_ptr;
+		}
+
 	/* see if the service can have notifications sent out at this time */
-	if(check_time_against_period(current_time,svc->notification_period_ptr)==ERROR){
+	if(check_time_against_period(current_time,temp_period)==ERROR){
 
 		log_debug_info(DEBUGL_NOTIFICATIONS,1,"This service shouldn't have notifications sent out at this time.\n");
 

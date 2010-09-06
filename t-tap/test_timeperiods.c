aad130926dbfb6c4cb99e4a6dc2968957ab8b1ae
Ton Voon <tonvoon@mac.com>
2010-09-06 08:33:42 +0000

    Added testcases for timeperiod exclusion bugs (Jean Gabes)
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@1451 5f96b256-904b-4d8d-8c98-d829582c6739

--- t-tap/test_timeperiods.c
+++ t-tap/test_timeperiods.c
@@ -310,7 +310,7 @@ int main(int argc, char **argv){
 	int is_valid_time=0;
 	int iterations=1000;
 
-	plan_tests(6031);
+	plan_tests(6043);
 
 	/* reset program variables */
 	reset_variables();
@@ -399,18 +399,88 @@ int main(int argc, char **argv){
 		c++;
 	}
 
+
+
 	/* Tests around clock change going back for TZ=Europe/London. 1256511661 = Sun Oct  
 25 23:01:01 2009 */
+	/* A little trip to Paris*/
+	putenv("TZ=Europe/Paris");
+        tzset();
+
+
+	/* Timeperiod exclude tests, from Jean Gabes */
+	temp_timeperiod = find_timeperiod("Test_exclude");
+        ok(temp_timeperiod!=NULL, "ME: Testing Exclude timeperiod");
+	test_time=1278939600;
+	/* printf("Testing at time %s", ctime(&test_time)); */
+        is_valid_time = check_time_against_period(test_time, temp_timeperiod);
+        ok( is_valid_time==ERROR, "ME: 12 Jul 2010 15:00:00 - false" );
+
+        _get_next_valid_time(test_time, test_time, &chosen_valid_time, temp_timeperiod);
+	/* printf("JEAN: Got chosent time at %s", ctime(&chosen_valid_time)); */
+	todo_start("Bug in exclude");
+        ok( chosen_valid_time==1288103400, "ME: Next valid time=Tue Oct 26 16:30:00 2010");
+	todo_end();
+
+
+	temp_timeperiod = find_timeperiod("Test_exclude2");
+        ok(temp_timeperiod!=NULL, "ME: Testing Exclude timeperiod 2");
+	test_time=1278939600;
+	/* printf("Testing at time %s", ctime(&test_time)); */
+        is_valid_time = check_time_against_period(test_time, temp_timeperiod);
+        ok( is_valid_time==ERROR, "ME: 12 Jul 2010 15:00:00 - false" );
+        _get_next_valid_time(test_time, test_time, &chosen_valid_time, temp_timeperiod);
+	/* printf("JEAN: Got chosent time at %s", ctime(&chosen_valid_time)); */
+	todo_start("Bug in exclude 2");
+        ok( chosen_valid_time==1279058340, "ME: Next valid time=Tue Jul 13 23:59:00 2010");
+	todo_end();
+
+
+	temp_timeperiod = find_timeperiod("Test_exclude3");
+        ok(temp_timeperiod!=NULL, "ME: Testing Exclude timeperiod 3");
+        test_time=1278939600;
+        /* printf("Testing at time %s", ctime(&test_time)); */
+        is_valid_time = check_time_against_period(test_time, temp_timeperiod);
+        ok( is_valid_time==ERROR, "ME: 12 Jul 2010 15:00:00 - false" );
+        _get_next_valid_time(test_time, test_time, &chosen_valid_time, temp_timeperiod);
+        /* printf("JEAN: Got chosent time at %s", ctime(&chosen_valid_time)); */
+	todo_start("Bug in exclude 3");
+        ok( chosen_valid_time==1284474600, "ME: Next valid time=Tue Sep 14 16:30:00 2010");
+	todo_end();
+
+
+	temp_timeperiod = find_timeperiod("Test_exclude4");
+        ok(temp_timeperiod!=NULL, "ME: Testing Exclude timeperiod 4");
+        test_time=1278939600;
+        /* printf("Testing at time %s", ctime(&test_time)); */
+        is_valid_time = check_time_against_period(test_time, temp_timeperiod);
+        ok( is_valid_time==ERROR, "ME: 12 Jul 2010 15:00:00 - false" );
+        _get_next_valid_time(test_time, test_time, &chosen_valid_time, temp_timeperiod);
+        /* printf("JEAN: Got chosent time at %s", ctime(&chosen_valid_time)); */
+	todo_start("Bug in exclude 3");
+        ok( chosen_valid_time==1283265000, "ME: Next valid time=Tue Aug 31 16:30:00 2010");
+	todo_end();
+
+
+	
+
+	/* Back to New york */
+	putenv("TZ=America/New_York");
+        tzset();
+
+
 	temp_timeperiod = find_timeperiod("sunday_only");
 	ok(temp_timeperiod!=NULL, "Testing Sunday 00:00-01:15,03:15-22:00");
 	putenv("TZ=Europe/London");
 	tzset();
+
 
 	test_time=1256421000;
 	is_valid_time = check_time_against_period(test_time, temp_timeperiod);
 	ok( is_valid_time==ERROR, "Sat Oct 24 22:50:00 2009 - false" );
 	_get_next_valid_time(test_time, test_time, &chosen_valid_time, temp_timeperiod);
 	ok( chosen_valid_time==1256425200, "Next valid time=Sun Oct 25 00:00:00 2009");
+
 
 	test_time=1256421661;
 	is_valid_time = check_time_against_period(test_time, temp_timeperiod);

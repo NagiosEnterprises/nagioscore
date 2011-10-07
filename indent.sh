ecd44735bcd4f1c2be5d867de2454ab2476e2fbf
Andreas Ericsson <ae@op5.se>
2011-10-07 14:29:21 +0000

    Make indent.sh a bit more flexible
    With this patch, multiple files can be passed to the indent.sh
    script and they'll all get indented. Still no change what so ever
    to how the "make indent" target works though.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>
    
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@1820 5f96b256-904b-4d8d-8c98-d829582c6739

--- indent.sh
+++ indent.sh
@@ -3,6 +3,4 @@ ARTISTIC_STYLE_OPTIONS=/dev/null
 
 export ARTISTIC_STYLE_OPTIONS
 
-f=$1
-
-astyle --style=banner --indent=tab --unpad-paren --pad-oper --suffix=none $f
+astyle --style=banner --indent=tab --unpad-paren --pad-oper --suffix=none "$@"

2e2b39c080b354c4abc60e034ae372bbf1568c66
Andreas Ericsson <ae@op5.se>
2012-09-16 15:01:15 +0000

    Stop reserving nebcallbacks we aren't using
    Since we're controlling the sources, we can easily add callbacks
    needed later, without tagging and reserving a bunch of them for no
    reason what so ever.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>
    
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@2227 5f96b256-904b-4d8d-8c98-d829582c6739

--- include/nebcallbacks.h
+++ include/nebcallbacks.h
@@ -30,41 +30,32 @@
 
 #define NEBCALLBACK_NUMITEMS                          33    /* total number of callback types we have */
 
-#define NEBCALLBACK_RESERVED0                         0     /* reserved for future use */
-#define NEBCALLBACK_RESERVED1                         1
-#define NEBCALLBACK_RESERVED2                         2
-#define NEBCALLBACK_RESERVED3                         3
-#define NEBCALLBACK_RESERVED4                         4
-
-#define NEBCALLBACK_RAW_DATA                          5
-#define NEBCALLBACK_NEB_DATA                          6
-
-#define NEBCALLBACK_PROCESS_DATA                      7
-#define NEBCALLBACK_TIMED_EVENT_DATA                  8
-#define NEBCALLBACK_LOG_DATA                          9
-#define NEBCALLBACK_SYSTEM_COMMAND_DATA               10
-#define NEBCALLBACK_EVENT_HANDLER_DATA                11
-#define NEBCALLBACK_NOTIFICATION_DATA                 12
-#define NEBCALLBACK_SERVICE_CHECK_DATA                13
-#define NEBCALLBACK_HOST_CHECK_DATA                   14
-#define NEBCALLBACK_COMMENT_DATA                      15
-#define NEBCALLBACK_DOWNTIME_DATA                     16
-#define NEBCALLBACK_FLAPPING_DATA                     17
-#define NEBCALLBACK_PROGRAM_STATUS_DATA               18
-#define NEBCALLBACK_HOST_STATUS_DATA                  19
-#define NEBCALLBACK_SERVICE_STATUS_DATA               20
-#define NEBCALLBACK_ADAPTIVE_PROGRAM_DATA             21
-#define NEBCALLBACK_ADAPTIVE_HOST_DATA                22
-#define NEBCALLBACK_ADAPTIVE_SERVICE_DATA             23
-#define NEBCALLBACK_EXTERNAL_COMMAND_DATA             24
-#define NEBCALLBACK_AGGREGATED_STATUS_DATA            25
-#define NEBCALLBACK_RETENTION_DATA                    26
-#define NEBCALLBACK_CONTACT_NOTIFICATION_DATA         27
-#define NEBCALLBACK_CONTACT_NOTIFICATION_METHOD_DATA  28
-#define NEBCALLBACK_ACKNOWLEDGEMENT_DATA              29
-#define NEBCALLBACK_STATE_CHANGE_DATA                 30
-#define NEBCALLBACK_CONTACT_STATUS_DATA               31
-#define NEBCALLBACK_ADAPTIVE_CONTACT_DATA             32
+#define NEBCALLBACK_PROCESS_DATA                      0
+#define NEBCALLBACK_TIMED_EVENT_DATA                  1
+#define NEBCALLBACK_LOG_DATA                          2
+#define NEBCALLBACK_SYSTEM_COMMAND_DATA               3
+#define NEBCALLBACK_EVENT_HANDLER_DATA                4
+#define NEBCALLBACK_NOTIFICATION_DATA                 5
+#define NEBCALLBACK_SERVICE_CHECK_DATA                6
+#define NEBCALLBACK_HOST_CHECK_DATA                   7
+#define NEBCALLBACK_COMMENT_DATA                      8
+#define NEBCALLBACK_DOWNTIME_DATA                     9
+#define NEBCALLBACK_FLAPPING_DATA                     10
+#define NEBCALLBACK_PROGRAM_STATUS_DATA               11
+#define NEBCALLBACK_HOST_STATUS_DATA                  12
+#define NEBCALLBACK_SERVICE_STATUS_DATA               13
+#define NEBCALLBACK_ADAPTIVE_PROGRAM_DATA             14
+#define NEBCALLBACK_ADAPTIVE_HOST_DATA                15
+#define NEBCALLBACK_ADAPTIVE_SERVICE_DATA             16
+#define NEBCALLBACK_EXTERNAL_COMMAND_DATA             17
+#define NEBCALLBACK_AGGREGATED_STATUS_DATA            18
+#define NEBCALLBACK_RETENTION_DATA                    19
+#define NEBCALLBACK_CONTACT_NOTIFICATION_DATA         20
+#define NEBCALLBACK_CONTACT_NOTIFICATION_METHOD_DATA  21
+#define NEBCALLBACK_ACKNOWLEDGEMENT_DATA              22
+#define NEBCALLBACK_STATE_CHANGE_DATA                 23
+#define NEBCALLBACK_CONTACT_STATUS_DATA               24
+#define NEBCALLBACK_ADAPTIVE_CONTACT_DATA             25
 
 
 /***** CALLBACK FUNCTIONS *****/

977e68bf6a86647d8a8348e06ec8d7e12c668632
Andreas Ericsson <ae@op5.se>
2012-10-22 10:33:09 +0000

    macros: Make cleanup rules humanly parseable
    The numerical values of macros really make no sense to use when
    humans are looking at them, so replace them with their macro names
    and use a case statement so we can grok at a glance which macros get
    which treatment.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>
    
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@2385 5f96b256-904b-4d8d-8c98-d829582c6739

--- common/macros.c
+++ common/macros.c
@@ -2540,13 +2540,27 @@ int init_macros(void) {
 		macro_keys[x].code = x;
 		macro_keys[x].name = macro_x_names[x];
 
-		/* host/service output/perfdata and author/comment macros should get cleaned */
-		if((x >= 16 && x <= 19) || (x >= 49 && x <= 52) || (x >= 99 && x <= 100) || (x >= 124 && x <= 127)) {
+		switch (x) {
+			/* output, perfdata, comments and author names need cleaning */
+		case MACRO_HOSTOUTPUT: case MACRO_SERVICEOUTPUT:
+		case MACRO_HOSTPERFDATA: case MACRO_SERVICEPERFDATA:
+		case MACRO_HOSTACKAUTHOR: case MACRO_HOSTACKCOMMENT:
+		case MACRO_SERVICEACKAUTHOR: case MACRO_SERVICEACKCOMMENT:
+		case MACRO_LONGHOSTOUTPUT: case MACRO_LONGSERVICEOUTPUT:
+		case MACRO_HOSTGROUPNOTES: case MACRO_SERVICEGROUPNOTES:
 			macro_keys[x].clean_options = (STRIP_ILLEGAL_MACRO_CHARS | ESCAPE_MACRO_CHARS);
-			}
-		/* url macros should get cleaned */
-		if((x >= 125 && x <= 126) || (x >= 128 && x <= 129) || (x >= 77 && x <= 78) || (x >= 74 && x <= 75)) {
+			break;
+
+			/* url macros get url-encoded */
+		case MACRO_HOSTACTIONURL: case MACRO_HOSTNOTESURL:
+		case MACRO_SERVICEACTIONURL: case MACRO_SERVICENOTESURL:
+		case MACRO_HOSTGROUPNOTESURL: case MACRO_HOSTGROUPACTIONURL:
+		case MACRO_SERVICEGROUPNOTESURL: case MACRO_SERVICEGROUPACTIONURL:
 			macro_keys[x].clean_options = URL_ENCODE_MACRO_CHARS;
+			break;
+		default:
+			macro_keys[x].clean_options = 0;
+			break;
 			}
 		}
 

cc4c128db0d6b33b9368d44b82bfc41d901d1e7e
Eric Stanley <estanley@nagios.com>
2013-02-02 13:07:54 +0000

    Added dropping privileges to core 4 ping worker template
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@2589 5f96b256-904b-4d8d-8c98-d829582c6739

--- worker/ping/worker-ping.c
+++ worker/ping/worker-ping.c
@@ -30,30 +30,44 @@
 #include "workers.h"
 
 /* Local macro definitions */
-#define PING_WORKER_VERSION "0.1"
-#define PING_WORKER_MODIFICATION_DATE "03-Jan-13"
+#define PING_WORKER_VERSION "0.2"
+#define PING_WORKER_MODIFICATION_DATE "02-Feb-13"
+
+#define PW_OK		0
+#define PW_ERROR	1
 
 /* Local function declarations */
 void		print_license( void);
 void		print_usage( char *);
 void		print_version( void);
-void		parse_worker_command_line( int, char **, char **, int *, char **);
+void		parse_worker_command_line( int, char **, char **, int *, char **,
+					char **, char **);
 static int	worker( const char *);
 
 /* Everything starts here */
 main( int argc, char **argv, char **env) {
 	int daemon_mode = FALSE;
 	char *worker_socket;
+	char *worker_user = ( char *)0;
+	char *worker_group = ( char *)0;
 
-	printf( "Greetings from the ping worker.\n");
+	if( FALSE == daemon_mode) {
+		printf( "Greetings from the ping worker.\n");
+	}
 
-	parse_worker_command_line( argc, argv, env, &daemon_mode, &worker_socket);
+	parse_worker_command_line( argc, argv, env, &daemon_mode, &worker_socket,
+			&worker_user, &worker_group);
 
 	if( FALSE == daemon_mode) {
 		print_version();
+		printf( "Worker socket is %s.\n", worker_socket);
 	}
 
-	printf( "Worker socket is %s.\n", worker_socket);
+	/* drop privileges */
+	if( drop_privileges( worker_user, worker_group) == PW_ERROR) {
+		fprintf( stderr, "Failed to drop privileges. Aborting.\n");
+		exit( 1);
+	}
 
 	/* Enter the worker code */
 	if( worker( worker_socket)) {
@@ -62,7 +76,8 @@ main( int argc, char **argv, char **env) {
 }
 
 void parse_worker_command_line( int argc, char **argv, char **env, 
-		int *daemon_mode, char **worker_socket) {
+		int *daemon_mode, char **worker_socket, char **worker_user, 
+		char **worker_group) {
 	int c = 0;
 	int display_usage = FALSE;
 	int display_license = FALSE;
@@ -75,6 +90,8 @@ void parse_worker_command_line( int argc, char **argv, char **env,
 		{ "license", no_argument, 0, 'V'},
 		{ "daemon", no_argument, 0, 'd'},
 		{ "worker", required_argument, 0, 'W'},
+		{ "user", required_argument, 0, 'u'},
+		{ "group", required_argument, 0, 'g'},
 		{ 0, 0, 0, 0}
 	};
 #define getopt( argc, argv, o) getopt_long( argc, argv, o, long_options, &option_index)
@@ -82,7 +99,7 @@ void parse_worker_command_line( int argc, char **argv, char **env,
 
 	/* get all command line arguments */
 	while( 1) {
-		c = getopt( argc, argv, "+:hVdW:");
+		c = getopt( argc, argv, "+:hVdW:u:g:");
 
 		if( -1 == c || EOF == c) break;
 
@@ -105,6 +122,14 @@ void parse_worker_command_line( int argc, char **argv, char **env,
 				*worker_socket = optarg;
 				break;
 
+			case 'u':
+				*worker_user = optarg;
+				break;
+
+			case 'g':
+				*worker_group = optarg;
+				break;
+
 			case ':':
 				printf( "Missing argument for command line option '%c'.\n\n", 
 						optopt);
@@ -120,13 +145,13 @@ void parse_worker_command_line( int argc, char **argv, char **env,
 		}
 	}
 
-	if( display_license == TRUE) {
+	if( TRUE == display_license) {
 		print_version();
 		print_license();
 		exit( 0);
 	}
 
-	if( display_usage == TRUE) {
+	if( TRUE == display_usage) {
 		print_usage( argv[ 0]);
 		exit( 0);
 	}
@@ -148,13 +173,15 @@ void print_license( void) {
 
 void print_usage( char *program_name) {
 	printf( "\nUsage: %s [-?|-h|--help] [-V|--version] [-d]\n", program_name);
-	printf( "        -W /path/to/socket\n");
+	printf( "        -W /path/to/socket -u user -g group\n");
 	printf( "\n");
 	printf( "Options:\n");
 	printf( "\n");
 	printf( "  -d, --daemon                 Starts the ping worker in daemon mode,\n");
 	printf( "                               instead of as a foreground process\n");
 	printf( "  -W, --worker /path/to/socket Act as a worker for an already running daemon\n");
+	printf( "  -u, --user user              Drop priviliges to the specified user\n");
+	printf( "  -g, --group group            Drop priviliges to the specified user\n");
 	printf( "\n");
 	printf( "Visit the Nagios website at http://www.nagios.org/ for bug fixes, new\n");
 	printf( "releases, online documentation, FAQs, information on subscribing to\n");
@@ -205,4 +232,96 @@ static int worker( const char *path)
 
 	enter_worker( sd, start_cmd);
 	return 0;
+}
+
+int drop_privileges( char *user, char *group) {
+	uid_t uid = -1;
+	gid_t gid = -1;
+	struct group *grp = NULL;
+	struct passwd *pw = NULL;
+	int result = PW_OK;
+
+	/* only drop privileges if we're running as root, so we don't 
+		interfere with being debugged while running as some random user */
+	if( getuid() != 0) {
+		return PW_OK;
+	}
+
+	/* set effective group ID */
+	if( NULL != group) {
+
+		/* see if this is a group name */
+		if( strspn( group, "0123456789") < strlen( group)) {
+			grp = ( struct group *)getgrnam( group);
+			if( NULL != grp) {
+				gid = ( gid_t)(grp->gr_gid);
+			}
+			else {
+				fprintf( stderr, 
+					"Warning: Could not get group entry for '%s'\n", group);
+			}
+		}
+
+		/* else we were passed the GID */
+		else {
+			gid = ( gid_t)atoi( group);
+		}
+
+		/* set effective group ID if other than current EGID */
+		if( gid != getegid()) {
+			if( setgid( gid) == -1) {
+				fprintf( stderr, "Warning: Could not set effective GID=%d\n", 
+						( int)gid);
+				result = PW_ERROR;
+			}
+		}
+	}
+
+	/* set effective user ID */
+	if( NULL != user) {
+
+		/* see if this is a user name */
+		if( strspn( user, "0123456789") < strlen( user)) {
+			pw = ( struct passwd *)getpwnam( user);
+			if( NULL != pw) {
+				uid = ( uid_t)(pw->pw_uid);
+			}
+			else {
+				fprintf( stderr, 
+						"Warning: Could not get passwd entry for '%s'\n", user);
+			}
+		}
+
+		/* else we were passed the UID */
+		else {
+			uid = ( uid_t)atoi( user);
+		}
+
+#ifdef HAVE_INITGROUPS
+
+		if( uid != geteuid()) {
+
+			/* initialize supplementary groups */
+			if( initgroups( user, gid) == -1) {
+				if( EPERM == errno) {
+					fprintf( stderr, "Warning: Unable to change supplementary "
+							"groups using initgroups() -- I hope you know what "
+							"you're doing\n");
+				}
+				else {
+					fprintf( stderr, "Warning: Possibly root user failed "
+							"dropping privileges with initgroups()\n");
+					return PW_ERROR;
+				}
+			}
+		}
+#endif
+		if( setuid( uid) == -1) {
+			fprintf( stderr, "Warning: Could not set effective UID=%d\n", 
+					( int)uid);
+			result = PW_ERROR;
+		}
+	}
+
+	return result;
 }

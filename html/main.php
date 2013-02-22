dfbfc0ce49625eaa4335f86849771d3c9e6cd329
Eric Stanley <estanley@nagios.com>
2013-02-22 19:29:34 +0000

    Update version number for 3.4.5rc1
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/branches/nagios-3-4-x@2626 5f96b256-904b-4d8d-8c98-d829582c6739

--- html/main.php
+++ html/main.php
@@ -1,7 +1,7 @@
 <?php
 include_once(dirname(__FILE__).'/includes/utils.inc.php');
 
-$this_version="3.4.4";
+$this_version="3.4.5rc1";
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
@@ -38,9 +38,9 @@ $this_version="3.4.4";
 
 <div id="currentversioninfo">
 <div class="product">Nagios<sup><span style="font-size: small;">&reg;</span></sup> Core<sup><span style="font-size: small;">&trade;</span></sup></div>
-<div class="version">Version 3.4.4</div>
-<div class="releasedate">January 12, 2013</div>
-<div class="checkforupdates"><a href="http://www.nagios.org/checkforupdates/?version=3.4.4&product=nagioscore" target="_blank">Check for updates</a></div>
+<div class="version">Version 3.4.5rc1</div>
+<div class="releasedate">February 22, 2013</div>
+<div class="checkforupdates"><a href="http://www.nagios.org/checkforupdates/?version=3.4.5rc1&product=nagioscore" target="_blank">Check for updates</a></div>
 <!--<div class="whatsnew"><a href="http://go.nagios.com/nagioscore/whatsnew">Read what's new in Nagios Core 3</a></div>-->
 </div>
 
--- nagios.spec
+++ nagios.spec
@@ -1,5 +1,5 @@
 %define name nagios
-%define version 3.4.4
+%define version 3.4.5rc1
 %define release 1
 %define nsusr nagios
 %define nsgrp nagios

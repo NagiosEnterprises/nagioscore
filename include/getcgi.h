66f82e6b28e917bb3f26a8700c6d6e8dec180dcd
Eric Stanley <estanley@nagios.com>
2013-02-22 16:01:06 +0000

    Updated CGI utility functions to support UTF-8 characters
    Specifically the escape_string() function in cgiutils.c was updated. 
    In order to support this we needed to set the encoding which is set 
    using setlocale(). 
    
    Because setlocale() specifies both the language and the encoding, 
    the function that reads the CGI variables now calls a function to 
    parse the HTTP_ACCEPT_LANGUAGE variable and attempt to set the 
    locale based on the user's preferred language and locality.
    
    This wasn't really necessary because all we really needed was to 
    set the encoding to UTF-8, but it may be useful in the future and 
    is less ethnocentric than just blindly setting the locale to 
    en_US.utf8. :-)
    
    
    
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@2622 5f96b256-904b-4d8d-8c98-d829582c6739

--- include/getcgi.h
+++ include/getcgi.h
@@ -8,10 +8,32 @@
 #include "lib/lnag-utils.h"
 NAGIOS_BEGIN_DECL
 
+#define ACCEPT_LANGUAGE_Q_DELIMITER	";q="
+
+/* information for a single language in the variable HTTP_ACCEPT_LANGUAGE 
+	sent by the browser */
+typedef struct accept_language_struct {
+	char *	language;
+	char *	locality;
+	double	q;
+} accept_language;
+
+/* information for all languages in the variable HTTP_ACCEPT_LANGUAGE 
+	sent by the browser */
+typedef struct accept_languages_struct {
+	int					count;
+	accept_language **	languages;
+} accept_languages;
+
 char **getcgivars(void);
 void free_cgivars(char **);
 void unescape_cgi_input(char *);
 void sanitize_cgi_input(char **);
 unsigned char hex_to_char(char *);
+
+void	process_language( char *);
+accept_languages *	parse_accept_languages( char *);
+static int compare_accept_languages( const void *, const void *);
+void	free_accept_languages( accept_languages *);
 
 NAGIOS_END_DECL

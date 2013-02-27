aba26a447af261e477a4beed90c7b11e814be045
Andreas Ericsson <ae@op5.se>
2013-02-27 14:49:53 +0000

    lib/test-runcmd: Add anomaly detection tests
    These are the tests responsible for the earlier flurry of commits.
    Since they've already proven useful, we keep them around in an
    easily runnable state.
    
    While at it, rename "runcmd test" to "exec output comparison".
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>
    
    git-svn-id: https://nagios.svn.sourceforge.net/svnroot/nagios/nagioscore/trunk@2642 5f96b256-904b-4d8d-8c98-d829582c6739

--- lib/test-runcmd.c
+++ lib/test-runcmd.c
@@ -30,11 +30,44 @@ struct cases cases[] = {
 	{NULL},
 };
 
+struct {
+	int ret;
+	char *cmd;
+} anomaly[] = {
+	{ CMD_HAS_REDIR, "cat lala | grep foo" },
+	{ 0, "cat lala \\| grep foo" },
+	{ CMD_HAS_JOBCONTROL, "foo && bar" },
+	{ 0, "foo \\&\\& bar" },
+	{ CMD_HAS_JOBCONTROL, "foo & bar" },
+	{ 0, "foo \\& bar" },
+	{ CMD_HAS_SUBCOMMAND, "foo \"`extcmd`\"" },
+	{ 0, "foo \"\\`extcmd\\`\"" },
+	{ CMD_HAS_SUBCOMMAND, "foo `extcmd`" },
+	{ 0, "foo \\`extcmd\\`" },
+	{ CMD_HAS_SUBCOMMAND, "foo \"$(extcmd)\"" },
+	{ 0, "foo \\$\\(extcmd\\)" },
+	{ CMD_HAS_SUBCOMMAND | CMD_HAS_PAREN, "foo $(extcmd" },
+	{ 0, "foo \\$\\(extcmd\\)" },
+	{ CMD_HAS_UBDQ, "foo\" bar" },
+	{ 0, "foo\\\" bar" },
+	{ CMD_HAS_UBSQ, "foo' bar" },
+	{ 0, "foo\\' bar" },
+	{ CMD_HAS_WILDCARD, "ls -l /tmp/*" },
+	{ 0, "ls -l /tmp/\\*" },
+	{ CMD_HAS_WILDCARD, "ls -l /dev/tty?" },
+	{ 0, "ls -l /dev/tty\\?" },
+	{ CMD_HAS_SHVAR, "echo $foo" },
+	{ 0, "echo \\$foo" },
+	{ 0, NULL},
+};
+
 int main(int argc, char **argv)
 {
+	int ret, r2;
+
 	runcmd_init();
 	t_set_colors(0);
-	t_start("runcmd test");
+	t_start("exec output comparison");
 	{
 		int i;
 		char *out = calloc(1, BUF_SIZE);
@@ -52,6 +85,18 @@ int main(int argc, char **argv)
 			close(fd);
 		}
 	}
-	t_end();
-	return t_end();
+	ret = t_end();
+	t_reset();
+	t_start("anomaly detection");
+	{
+		int i;
+		for (i = 0; anomaly[i].cmd; i++) {
+			int out_argc;
+			char *out_argv[256];
+			int ret = runcmd_cmd2strv(anomaly[i].cmd, &out_argc, out_argv);
+			ok_int(ret, anomaly[i].ret, anomaly[i].cmd);
+		}
+	}
+	r2 = t_end();
+	return r2 ? r2 : ret;
 }

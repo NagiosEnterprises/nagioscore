0d2c1257c0bea400cd58336d7f298c43b36e9cfd
Andreas Ericsson <ae@op5.se>
2013-07-30 10:21:22 +0200

    daemon: Make query socket path main-config-relative
    This just brings it in line with all the other path-variables and
    makes testing a little bit easier.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- base/config.c
+++ base/config.c
@@ -179,8 +179,10 @@ int read_main_config_file(char *main_config_file) {
 			error = set_loadctl_options(value, strlen(value)) != OK;
 		else if(!strcmp(variable, "check_workers"))
 			num_check_workers = atoi(value);
-		else if(!strcmp(variable, "query_socket"))
-			qh_socket_path = (char *)strdup(value);
+		else if(!strcmp(variable, "query_socket")) {
+			my_free(qh_socket_path);
+			qh_socket_path = nspath_absolute(value, config_file_dir);
+		}
 		else if(!strcmp(variable, "log_file")) {
 
 			if(strlen(value) > MAX_FILENAME_LENGTH - 1) {

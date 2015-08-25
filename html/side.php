d09c206ab1fb3e2af11ec13b268799640fd4175a
John C. Frickson <jfrickson@nagios.com>
2015-08-25 12:07:07 -0500

    Prep for version 4.1.2-beta

--- html/side.php
+++ html/side.php
@@ -1,7 +1,7 @@
 <?php
 include_once(dirname(__FILE__).'/includes/utils.inc.php');
 
-$this_version = '4.1.1';
+$this_version = '4.1.2-beta';
 $link_target = 'main';
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
--- nagios.spec
+++ nagios.spec
@@ -21,7 +21,7 @@
 
 Summary: Open Source host, service and network monitoring program
 Name: nagios
-Version: 4.1.1
+Version: 4.1.2-beta
 Release: 2%{?dist}
 License: GPL
 Group: Applications/System

0cb5803c31f198d9ddbbce82332ba14bd32fe8e7
Bryan Heden <b.heden@gmail.com>
2016-06-17 13:42:57 -0500

    Update utils.c
    Updated errno to be wrapped in string.

--- base/utils.c
+++ base/utils.c
@@ -554,7 +554,7 @@ int my_system_r(nagios_macros *mac, char *cmd, int timeout, int *early_timeout, 
 
 	/* return an error if we couldn't fork */
 	if(pid == -1) {
-		logit(NSLOG_RUNTIME_WARNING, TRUE, "Warning: fork() in my_system_r() failed for command \"%s\"\n", cmd);
+		logit(NSLOG_RUNTIME_WARNING, TRUE, "Warning: fork() in my_system_r() failed for command \"%s\" - errno: %s\n", cmd, strerror(errno));
 
 		/* close both ends of the pipe */
 		close(fd[0]);

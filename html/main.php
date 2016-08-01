b9751c21db79353ab95d6d4401b129a048657281
John C. Frickson <jfrickson@nagios.com>
2016-08-01 10:14:58 -0500

    Prep for release 4.2.0

--- html/main.php
+++ html/main.php
@@ -1,7 +1,7 @@
 <?php
 include_once(dirname(__FILE__).'/includes/utils.inc.php');
 
-$this_version = '4.1.2-RC2';
+$this_version = '4.2.0';
 $this_year = '2016';
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
@@ -129,7 +129,7 @@ $this_year = '2016';
 <div id="currentversioninfo">
 	<div class="product">Nagios<sup><span style="font-size: small;">&reg;</span></sup> Core<sup><span style="font-size: small;">&trade;</span></sup></div>
 	<div class="version">Version <?php echo $this_version; ?></div>
-	<div class="releasedate">July 19, 2016</div>
+	<div class="releasedate">August 01, 2016</div>
 	<div class="checkforupdates"><a href="https://www.nagios.org/checkforupdates/?version=<?php echo $this_version; ?>&amp;product=nagioscore" target="_blank">Check for updates</a></div>
 </div>
 

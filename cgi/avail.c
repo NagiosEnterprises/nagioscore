7206ededddf084b22a344865050e282963874bc1
John C. Frickson <jfrickson@nagios.com>
2016-08-29 15:57:37 -0500

    Servicegroup availability report ignores includesoftstates
    in service report links
    
    Fix for issue http://tracker.nagios.org/view.php?id=551
    
    Patch from PriceChild applied

--- cgi/avail.c
+++ cgi/avail.c
@@ -4723,6 +4723,8 @@ void service_report_url(const char *hn, const char *sd, const char *label) {
 	printf("&showscheduleddowntime=%s", (show_scheduled_downtime == TRUE) ? "yes" : "no");
 	if(current_timeperiod != NULL)
 		printf("&rpttimeperiod=%s", url_encode(current_timeperiod->name));
+	if(include_soft_states != NULL)
+		printf("&includesoftstates=%s", (include_soft_states == TRUE) ? "yes" : "no");
 	printf("'>%s</a>", label);
 
 	return;

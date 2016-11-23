3c8b88d68a8eefe9effb81686d22726edc6a29c6
Daniel Svoboda <daniel.svoboda@coservit.fr>
2016-11-23 15:54:19 +0100

    Corriger la gestion de type d'état SOFT->HARD pendant la passage d'état d'un équipement UP->DOWN/UNREACHABLE
    VSWA-7479
    
    - corrigé la gestion de type d'état SOFT->HARD
    - augmenté la version de Nagios a 4.2.3.1

--- html/main.php
+++ html/main.php
@@ -1,7 +1,7 @@
 <?php
 include_once(dirname(__FILE__).'/includes/utils.inc.php');
 
-$this_version = '4.2.3';
+$this_version = '4.2.3.1';
 $this_year = '2016';
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
@@ -129,7 +129,7 @@ $this_year = '2016';
 <div id="currentversioninfo">
 	<div class="product">Nagios<sup><span style="font-size: small;">&reg;</span></sup> Core<sup><span style="font-size: small;">&trade;</span></sup></div>
 	<div class="version">Version <?php echo $this_version; ?></div>
-	<div class="releasedate">November 21, 2016</div>
+	<div class="releasedate">November 23, 2016</div>
 	<div class="checkforupdates"><a href="https://www.nagios.org/checkforupdates/?version=<?php echo $this_version; ?>&amp;product=nagioscore" target="_blank">Check for updates</a></div>
 </div>
 

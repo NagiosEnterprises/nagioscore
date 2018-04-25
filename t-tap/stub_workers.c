f0726b8b7a6949bf88c4388610992e9689d40c95
Bryan Heden <bheden@nagios.com>
2018-04-25 18:53:51 -0500

    first update to fix `make test` in a while, i bet

--- t-tap/stub_workers.c
+++ t-tap/stub_workers.c
@@ -2,3 +2,4 @@
 int wproc_run_check(check_result *cr, char *cmd, nagios_macros *mac) { return OK; }
 int wproc_can_spawn(struct load_control *lc) { return 1; }
 void wproc_reap(int jobs, int msecs) {}
+int get_desired_workers(int desired_workers) { return 4; }

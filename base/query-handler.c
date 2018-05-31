b95a9846ec48351ce4d8941a4f72f24582bbdd3a
Bryan Heden <bheden@nagios.com>
2018-05-31 15:34:16 -0500

    wrong type

--- base/query-handler.c
+++ base/query-handler.c
@@ -36,7 +36,7 @@ static int qh_echo(int sd, char *buf, unsigned int len)
 	result = write(sd, buf, len);
 	if (result == -1) {
 
-		logit(NSLOG_RUNTIME_ERROR, TRUE, "qh: qh_echo() error on write(sd,buf=[%s],len=%lu): %s\n", buf, len, strerror(errno));
+		logit(NSLOG_RUNTIME_ERROR, TRUE, "qh: qh_echo() error on write(sd,buf=[%s],len=%d): %s\n", buf, len, strerror(errno));
 	}
 	return 0;
 }

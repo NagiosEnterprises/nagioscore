a2ddb2178e50b91bad6743f1d894fe8eedf00b46
Bryan Heden <bheden@nagios.com>
2018-06-06 11:59:39 -0500

    Fixed clear_host/service_flapping command logic to broker/notify properly (#525)

--- base/commands.c
+++ base/commands.c
@@ -5303,29 +5303,25 @@ void set_service_notification_number(service *svc, int num) {
 /* clears the flapping state for a specific host */
 void clear_host_flapping_state(host *hst) {
 
-	int		i;
-	double	low_threshold = 0.0;
-	double	high_threshold = 0.0;
+	int i                 = 0;
+	double low_threshold  = 0.0;
+	double high_threshold = 0.0;
 
-	if (enable_flap_detection == FALSE || hst->flap_detection_enabled == FALSE)
+	if (enable_flap_detection == FALSE || hst->flap_detection_enabled == FALSE) {
 		return;
+	}
 
 	low_threshold = (hst->low_flap_threshold <= 0.0) ? low_host_flap_threshold : hst->low_flap_threshold;
 	high_threshold = (hst->high_flap_threshold <= 0.0) ? high_host_flap_threshold : hst->high_flap_threshold;
 
-	for (i = 0; i < MAX_STATE_HISTORY_ENTRIES; ++i)
-		hst->state_history[i] = 0;			/* clear the history */
-	hst->state_history_index = 0;
-	hst->percent_state_change = 0.0;
-	if(hst->flapping_comment_id != 0)	/* delete the comment we added earlier */
-		delete_host_comment(hst->flapping_comment_id);
-	hst->flapping_comment_id = 0;
-	hst->is_flapping = FALSE;			/* clear the flapping indicator */
-
 	if (hst->is_flapping) {
+
 		log_debug_info(DEBUGL_FLAPPING, 1, "Host '%s' flapping state cleared.\n", hst->name);
+
 		/* log a notice - this one is parsed by the history CGI */
-		logit(NSLOG_INFO_MESSAGE, FALSE, "HOST FLAPPING ALERT: %s;STOPPED; Flapping state cleared for host. (%2.1f%% change < %2.1f%% threshold)\n", hst->name, hst->percent_state_change, low_threshold);
+		logit(NSLOG_INFO_MESSAGE, FALSE, 
+			"HOST FLAPPING ALERT: %s;STOPPED; Flapping state cleared for host. (%2.1f%% change < %2.1f%% threshold)\n", 
+			hst->name, hst->percent_state_change, low_threshold);
 
 #ifdef USE_EVENT_BROKER
 		/* send data to event broker */
@@ -5336,9 +5332,26 @@ void clear_host_flapping_state(host *hst) {
 		host_notification(hst, NOTIFICATION_FLAPPINGSTOP, NULL, NULL, NOTIFICATION_OPTION_NONE);
 
 		/* should we send a recovery notification? */
-		if(hst->check_flapping_recovery_notification == TRUE && hst->current_state == HOST_UP)
+		if (hst->current_state == HOST_UP && hst->check_flapping_recovery_notification == TRUE) {
 			host_notification(hst, NOTIFICATION_NORMAL, NULL, NULL, NOTIFICATION_OPTION_NONE);
+		}
 	}
+
+	/* clear the history and state change percent */
+	for (i = 0; i < MAX_STATE_HISTORY_ENTRIES; ++i) {
+		hst->state_history[i] = 0;
+	}
+	hst->state_history_index = 0;
+	hst->percent_state_change = 0.0;
+
+	/* delete the comment we added earlier */
+	if (hst->flapping_comment_id != 0) {
+		delete_host_comment(hst->flapping_comment_id);
+	}
+	hst->flapping_comment_id = 0;
+
+	/* clear the flapping indicator */
+	hst->is_flapping = FALSE;
 
 	/* clear the recovery notification flag */
 	hst->check_flapping_recovery_notification = FALSE;
@@ -5347,29 +5360,25 @@ void clear_host_flapping_state(host *hst) {
 /* clears the flapping state for a specific service */
 void clear_service_flapping_state(service *svc) {
 
-	int		i;
-	double	low_threshold = 0.0;
-	double	high_threshold = 0.0;
+	int i                 = 0;
+	double low_threshold  = 0.0;
+	double high_threshold = 0.0;
 
-	if (enable_flap_detection == FALSE || svc->flap_detection_enabled == FALSE)
+	if (enable_flap_detection == FALSE || svc->flap_detection_enabled == FALSE) {
 		return;
+	}
 
 	low_threshold = (svc->low_flap_threshold <= 0.0) ? low_service_flap_threshold : svc->low_flap_threshold;
 	high_threshold = (svc->high_flap_threshold <= 0.0) ? high_service_flap_threshold : svc->high_flap_threshold;
 
-	for (i = 0; i < MAX_STATE_HISTORY_ENTRIES; ++i)
-		svc->state_history[i] = 0;			/* clear the history */
-	svc->state_history_index = 0;
-	svc->percent_state_change = 0.0;
-	if(svc->flapping_comment_id != 0)	/* delete the comment we added earlier */
-		delete_service_comment(svc->flapping_comment_id);
-	svc->flapping_comment_id = 0;
-	svc->is_flapping = FALSE;			/* clear the flapping indicator */
-
 	if (svc->is_flapping) {
+
 		log_debug_info(DEBUGL_FLAPPING, 1, "Service '%s' on host '%s' flapping state cleared.\n", svc->description, svc->host_name);
+
 		/* log a notice - this one is parsed by the history CGI */
-		logit(NSLOG_INFO_MESSAGE, FALSE, "SERVICE FLAPPING ALERT: %s;%s;STOPPED; Flapping state cleared for service. (%2.1f%% change < %2.1f%% threshold)\n", svc->host_name, svc->description, svc->percent_state_change, low_threshold);
+		logit(NSLOG_INFO_MESSAGE, FALSE, 
+			"SERVICE FLAPPING ALERT: %s;%s;STOPPED; Flapping state cleared for service. (%2.1f%% change < %2.1f%% threshold)\n", 
+			svc->host_name, svc->description, svc->percent_state_change, low_threshold);
 
 #ifdef USE_EVENT_BROKER
 		/* send data to event broker */
@@ -5380,9 +5389,26 @@ void clear_service_flapping_state(service *svc) {
 		service_notification(svc, NOTIFICATION_FLAPPINGSTOP, NULL, NULL, NOTIFICATION_OPTION_NONE);
 
 		/* should we send a recovery notification? */
-		if(svc->check_flapping_recovery_notification == TRUE && svc->current_state == STATE_OK)
+		if (svc->current_state == STATE_OK && svc->check_flapping_recovery_notification == TRUE) {
 			service_notification(svc, NOTIFICATION_NORMAL, NULL, NULL, NOTIFICATION_OPTION_NONE);
+		}
 	}
+
+	/* clear the history and state change percent */
+	for (i = 0; i < MAX_STATE_HISTORY_ENTRIES; ++i) {
+		svc->state_history[i] = 0;
+	}
+	svc->state_history_index = 0;
+	svc->percent_state_change = 0.0;
+
+	/* delete the comment we added earlier */
+	if (svc->flapping_comment_id != 0) {
+		delete_service_comment(svc->flapping_comment_id);
+	}
+	svc->flapping_comment_id = 0;
+
+	/* clear the flapping indicator */
+	svc->is_flapping = FALSE;
 
 	/* clear the recovery notification flag */
 	svc->check_flapping_recovery_notification = FALSE;

ab03e0268336644a7d08c95f50e71bc1f2d85a26
jomann09 <jomann@nagios.com>
2019-01-07 01:09:25 -0600

    Revert "Support for customizing side.php with quicklinks (#530)"
    This reverts commit f9afa6515cad7b1763a18a3fd6ea115ce2f44244.

--- html/side.php
+++ html/side.php
@@ -32,7 +32,6 @@ $link_target = 'main';
 </div>
 
 <div class="navsection">
-	<?php include_once(dirname(__FILE__).'/quicklinks.php');?>
 	<div class="navsectiontitle">Current Status</div>
 	<div class="navsectionlinks">
 		<ul class="navsectionlinks">

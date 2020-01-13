NagiosEnterprises/nagioscore

commit 4b0d6c999625fe42aad4827b4f577c3d2ee2931f

Added unique identifer for objects into the object itself
Previously the when Nagios objects in the JSON object were indexed
by their unique identifier (usually a name, but sometimes an ID),
the unique identifier was left out of the JSON object. Now it is
included.

Thanks to Andy Brist <abrist@nagios.com> for this suggestion.

cgi/statusjson.c
@@ -1942,6 +1942,7 @@ json_object *json_status_host(unsigned format_options, host *temp_host,
 void json_status_host_details(json_object *json_details, unsigned format_options, 
 		host *temp_host, hoststatus *temp_hoststatus) {
 
+	json_object_append_string(json_details, "name", temp_host->name);
 	json_object_append_string(json_details, "plugin_output", 
 			temp_hoststatus->plugin_output);
 	json_object_append_string(json_details, "long_plugin_output", 
@@ -2502,7 +2503,8 @@ json_object *json_status_service(unsigned format_options, service *temp_service,
 	json_object *json_service = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_string(json_details, "host_name", temp_service->host_name);
+	json_object_append_string(json_details, "host_name", 
+			temp_service->host_name);
 	json_object_append_string(json_details, "description", 
 			temp_service->description);
 	json_status_service_details(json_details, format_options, temp_service, 
@@ -2516,6 +2518,10 @@ void json_status_service_details(json_object *json_details,
 		unsigned format_options, service *temp_service, 
 		servicestatus *temp_servicestatus) {
 
+	json_object_append_string(json_details, "host_name", 
+			temp_service->host_name);
+	json_object_append_string(json_details, "description", 
+			temp_service->description);
 	json_object_append_string(json_details, "plugin_output", 
 			temp_servicestatus->plugin_output);
 	json_object_append_string(json_details, "long_plugin_output", 
@@ -2770,6 +2776,8 @@ json_object *json_status_comment(unsigned format_options, comment *temp_comment)
 void json_status_comment_details(json_object *json_details, 
 		unsigned format_options, comment *temp_comment) {
 
+	json_object_append_integer(json_details, "comment_id", 
+			temp_comment->comment_id);
 	json_enumeration(json_details, format_options, "comment_type",
 			temp_comment->comment_type, svm_comment_types);
 	json_enumeration(json_details, format_options, "entry_type",
@@ -2972,6 +2980,8 @@ json_object *json_status_downtime(unsigned format_options,
 void json_status_downtime_details(json_object *json_details, 
 		unsigned format_options, scheduled_downtime *temp_downtime) {
 
+	json_object_append_integer(json_details, "downtime_id", 
+			temp_downtime->downtime_id);
 	json_enumeration(json_details, format_options, "type", temp_downtime->type, 
 			svm_downtime_types);
 	json_object_append_string(json_details, "host_name", 

cgi/objectjson.c
@@ -1790,6 +1790,7 @@ void json_object_host_details(json_object *json_details, unsigned format_options
 #endif
 	customvariablesmember *temp_custom_variablesmember;
 
+	json_object_append_string(json_details, "name", temp_host->name);
 	json_object_append_string(json_details, "display_name", 
 			temp_host->display_name);
 	json_object_append_string(json_details, "alias", temp_host->alias);
@@ -2186,7 +2187,8 @@ json_object *json_object_hostgroup(unsigned format_options,
 	json_object *json_hostgroup = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_string(json_details, "name", temp_hostgroup->group_name);
+	json_object_append_string(json_details, "group_name", 
+			temp_hostgroup->group_name);
 	json_object_hostgroup_details(json_details, format_options, temp_hostgroup);
 	json_object_append_object(json_hostgroup, "hostgroup", json_details);
 
@@ -2199,6 +2201,8 @@ void json_object_hostgroup_details(json_object *json_details,
 	json_array *json_members;
 	hostsmember *temp_member;
 
+	json_object_append_string(json_details, "group_name", 
+			temp_hostgroup->group_name);
 	json_object_append_string(json_details, "alias", temp_hostgroup->alias);
 
 	json_members = json_new_array();
@@ -2488,7 +2492,8 @@ json_object *json_object_service(unsigned format_options, service *temp_service)
 	json_object *json_service = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_string(json_details, "host_name", temp_service->host_name);
+	json_object_append_string(json_details, "host_name", 
+			temp_service->host_name);
 	json_object_append_string(json_details, "description", 
 			temp_service->description);
 	json_object_service_details(json_details, format_options, temp_service);
@@ -2516,6 +2521,10 @@ void json_object_service_details(json_object *json_details,
 	json_array *json_custom_variables;
 	customvariablesmember *temp_customvariablesmember;
 
+	json_object_append_string(json_details, "host_name", 
+			temp_service->host_name);
+	json_object_append_string(json_details, "description", 
+			temp_service->description);
 	json_object_append_string(json_details, "display_name", 
 			temp_service->display_name);
 #ifdef JSON_NAGIOS_4X
@@ -2928,7 +2937,7 @@ json_object * json_object_servicegroup(unsigned format_options,
 	json_object *json_servicegroup = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_string(json_details, "name", 
+	json_object_append_string(json_details, "group_name", 
 			temp_servicegroup->group_name);
 	json_object_servicegroup_details(json_details, format_options, 
 			temp_servicegroup);
@@ -2944,6 +2953,8 @@ void json_object_servicegroup_details(json_object *json_details,
 	servicesmember *temp_member;
 	json_object *json_member;
 
+	json_object_append_string(json_details, "group_name", 
+			temp_servicegroup->group_name);
 	json_object_append_string(json_details, "alias", temp_servicegroup->alias);
 
 	json_members = json_new_array();
@@ -3110,6 +3121,7 @@ void json_object_contact_details(json_object *json_details,
 	commandsmember *temp_commandsmember;
 	customvariablesmember *temp_customvariablesmember;
 
+	json_object_append_string(json_details, "name", temp_contact->name);
 	json_object_append_string(json_details, "alias", temp_contact->alias);
 	json_object_append_string(json_details, "email", temp_contact->email);
 	json_object_append_string(json_details, "pager", temp_contact->pager);
@@ -3384,7 +3396,7 @@ json_object *json_object_contactgroup(unsigned format_options,
 	json_object *json_contactgroup = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_string(json_details, "name", 
+	json_object_append_string(json_details, "group_name", 
 			temp_contactgroup->group_name);
 	json_object_contactgroup_details(json_details, format_options, 
 			temp_contactgroup);
@@ -3399,6 +3411,8 @@ void json_object_contactgroup_details(json_object *json_details,
 	json_array *json_members;
 	contactsmember *temp_member;
 
+	json_object_append_string(json_details, "group_name", 
+			temp_contactgroup->group_name);
 	json_object_append_string(json_details, "alias", temp_contactgroup->alias);
 
 	json_members = json_new_array();
@@ -3525,6 +3539,7 @@ void json_object_timeperiod_details(json_object *json_details,
 	daterange *temp_daterange;
 	timeperiodexclusion *temp_timeperiodexclusion;
 
+	json_object_append_string(json_details, "name", temp_timeperiod->name);
 	json_object_append_string(json_details, "alias", temp_timeperiod->alias);
 
 	json_days = json_new_object();
@@ -3533,7 +3548,7 @@ void json_object_timeperiod_details(json_object *json_details,
 			json_timeranges = json_new_array();
 			json_object_timerange(json_timeranges, format_options, 
 					temp_timeperiod->days[x]);
-			json_object_append_object(json_days, (char *)dayofweek[x], 
+			json_object_append_array(json_days, (char *)dayofweek[x], 
 					json_timeranges);
 			}
 		}
@@ -3838,6 +3853,7 @@ json_object *json_object_command(unsigned format_options, command *temp_command)
 
 void json_object_command_details(json_object *json_details, 
 		unsigned format_options, command *temp_command) {
+	json_object_append_string(json_details, "name", temp_command->name);
 	json_object_append_string(json_details, "command_line", 
 			temp_command->command_line);
 	}

commit 813a682f6bc8c6375d46c340691c9d5772f3979e

Fixes #718 for hosts/services/comments/downtime.
Issue seems like it was introduced in 4b0d6c999625fe42aad4827b4f577c3d2ee2931f
because the '_details' functions were being used in multiple places.

cgi/statusjson.c
@@ -2738,8 +2738,6 @@ json_object *json_status_host(unsigned format_options, host *temp_host,
 	json_object *json_host = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_string(json_details, "name", &percent_escapes,
-			temp_host->name);
 	json_status_host_details(json_details, format_options, temp_host, 
 			temp_hoststatus);
 	json_object_append_object(json_host, "host", json_details);
@@ -3460,10 +3458,6 @@ json_object *json_status_service(unsigned format_options, service *temp_service,
 	json_object *json_service = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_string(json_details, "host_name", &percent_escapes,
-			temp_service->host_name);
-	json_object_append_string(json_details, "description", &percent_escapes,
-			temp_service->description);
 	json_status_service_details(json_details, format_options, temp_service, 
 			temp_servicestatus);
 	json_object_append_object(json_service, "service", json_details);
@@ -3846,8 +3840,6 @@ json_object *json_status_comment(unsigned format_options,
 	json_object *json_comment = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_integer(json_details, "comment_id", 
-			temp_comment->comment_id);
 	json_status_comment_details(json_details, format_options, temp_comment); 
 	json_object_append_object(json_comment, "comment", json_details);
 
@@ -4171,8 +4163,6 @@ json_object *json_status_downtime(unsigned format_options,
 	json_object *json_downtime = json_new_object();
 	json_object *json_details = json_new_object();
 
-	json_object_append_integer(json_details, "downtime_id", 
-			temp_downtime->downtime_id);
 	json_status_downtime_details(json_details, format_options, temp_downtime); 
 	json_object_append_object(json_downtime, "downtime", json_details);
 
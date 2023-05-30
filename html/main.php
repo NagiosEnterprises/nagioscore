a063f2928e571ccc60d50fbbd483042c5fa951fa
Sebastian Wolf <swolf@nagios.com>
2023-05-30 15:13:47 -0400

    Prep for release 4.4.12 (switched date - forgot about memorial day)

--- html/main.php
+++ html/main.php
@@ -145,7 +145,7 @@ $this_year = '2023';
 <div id="currentversioninfo">
 	<div class="product">Nagios<sup><span style="font-size: small;">&reg;</span></sup> Core<sup><span style="font-size: small;">&trade;</span></sup></div>
 	<div class="version">Version <?php echo $this_version; ?></div>
-	<div class="releasedate">May 29, 2023</div>
+	<div class="releasedate">May 30, 2023</div>
 	<div class="checkforupdates"><a href="https://www.nagios.org/checkforupdates/?version=<?php echo $this_version; ?>&amp;product=nagioscore" target="_blank">Check for updates</a></div>
 </div>
 

cc0176bae732d0e9c9513fe7a4c79e90a1ea5ecb
Griffin Westerman <gwesterman@nagios.com>
2024-05-30 12:19:01 -0500

    fix: changed all instances of SCHEDULE_FORCED_SERVICE_NOTIFICATION to SCHEDULE_FORCED_SVC_NOTIFICATION

--- cgi/cmd.c
+++ cgi/cmd.c
@@ -2171,7 +2171,7 @@ int commit_command(int cmd) {
 		case CMD_SCHEDULE_FORCED_HOST_NOTIFICATION:
 			result = cmd_submitf(cmd, "%s;%lu", host_name, start_time);
 			break;
-		case CMD_SCHEDULE_FORCED_SERVICE_NOTIFICATION:
+		case CMD_SCHEDULE_FORCED_SVC_NOTIFICATION:
 			result = cmd_submitf(cmd, "%s;%s;%lu", host_name, service_desc, start_time);
 
 		case CMD_SEND_CUSTOM_HOST_NOTIFICATION:

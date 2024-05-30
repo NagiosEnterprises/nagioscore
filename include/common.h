72d7f652c05fbd4b230b9cf65b5789781567976c
Griffin Westerman <gwesterman@nagios.com>
2024-05-30 12:27:21 -0500

    chore: whitespace

--- include/common.h
+++ include/common.h
@@ -354,7 +354,7 @@ NAGIOS_END_DECL
 #define CMD_CHANGE_SVC_EVENT_HANDLER_TIMEPERIOD         176
 
 #define CMD_SCHEDULE_FORCED_HOST_NOTIFICATION           177
-#define CMD_SCHEDULE_FORCED_SVC_NOTIFICATION        178
+#define CMD_SCHEDULE_FORCED_SVC_NOTIFICATION            178
 
 /* custom command introduced in Nagios 3.x */
 #define CMD_CUSTOM_COMMAND                              999

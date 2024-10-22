NagiosEnterprises/nagioscore

commit e7a17b333187965686b404c01829ae0653f22530

cleaned up checks quite a bit, added inline functions for most of the logic blocks in an easy-to-digest fashion for code maintainability

include/nagios.h
@@ -559,14 +559,11 @@ void handle_service_flap_detection_disabled(service *);		/* handles the details 
 int check_host_check_viability(host *, int, int *, time_t *);
 int adjust_host_check_attempt(host *, int);
 int determine_host_reachability(host *);
-int process_host_check_result(host *, int, char *, int, int, int, unsigned long);
 int perform_on_demand_host_check(host *, int *, int, int, unsigned long);
 int execute_sync_host_check(host *);
 int run_scheduled_host_check(host *, int, double);
 int run_async_host_check(host *, int, double, int, int, int *, time_t *);
 int handle_async_host_check_result(host *, check_result *);
-double calculate_check_result_execution_time(check_result *);
-void record_last_service_state_ended(service *);
 
 
 /**** Service Check Functions ****/

commit 4fec3d862301f0c13f6ee204846b9cdc27cb9786

Fix number in header and fix bug introduced in e7a17b333187965686b404c01829ae0653f22530

base/checks.c
@@ -1064,8 +1064,8 @@ static inline void host_propagate_checks_to_immediate_parents(host * hst, int pa
 	log_debug_info(DEBUGL_CHECKS, 1, "Propagating checks to parent host(s)...\n");
 	for(temp_hostsmember = hst->parent_hosts; temp_hostsmember != NULL; temp_hostsmember = temp_hostsmember->next) {
 		parent_host = temp_hostsmember->host_ptr;
-		if ((parent_host_up == TRUE  && parent_host->current_state == HOST_UP) 
-			|| ((parent_host_up == FALSE && parent_host->current_state != HOST_UP))) {
+		if ((parent_host_up == TRUE  && parent_host->current_state != HOST_UP) 
+			|| ((parent_host_up == FALSE && parent_host->current_state == HOST_UP))) {
 
 			log_debug_info(DEBUGL_CHECKS, 1, "Check of parent host '%s' queued.\n", parent_host->name);
 			schedule_host_check(parent_host, current_time, CHECK_OPTION_DEPENDENCY_CHECK);

include/nagios.h
@@ -326,7 +326,7 @@ extern struct load_control loadctl;
 #define NOTIFICATION_DOWNTIMEEND        6
 #define NOTIFICATION_DOWNTIMECANCELLED  7
 #define NOTIFICATION_CUSTOM             8
-#define NOTIFICATION_RECOVERY           16
+#define NOTIFICATION_RECOVERY           9
 
 
 
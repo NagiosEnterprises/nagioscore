cffc7ae53db6de0051acdda6e325c40c7ad0ff4d
Dylan Anderson <tsadpbb@protonmail.com>
2024-10-23 11:31:00 -0500

    Change links

--- html/main.php
+++ html/main.php
@@ -104,13 +104,13 @@ $this_year = '2024';
 </div>
 
 <div id="mainsplash">
-	<a href="https://www.nagios.org/launch?utm_campaign=csp&utm_source=nagioscore&utm_medium=splash-thumbnail&utm_content=<?php echo $this_version; ?>" target="_blank"><img src="images/csp-dashboard.avif" /></a>
+	<a href="https://www.nagios.org/launch?utm_campaign=csp&utm_source=nagioscore&utm_medium=splash_image&utm_content=<?php echo $this_version; ?>" target="_blank"><img src="images/csp-dashboard.avif" /></a>
 	<div id="splashtext">
 		<div id="splashtexttitle">Meet Nagios Core Services Platform</div>
 		<div>The next generation of Open Source powered monitoring with advanced dashboards, monitoring wizards, and much more!</div>
 		<div id="splashbuttons">
-			<a id="splashlearnmore" href="https://www.nagios.org/launch?utm_campaign=csp&utm_source=nagioscore&utm_medium=learn-more&utm_content=<?php echo $this_version; ?>" target="_blank">Learn More</a>
-			<a id="splashnewsletter" href="https://www.nagios.org/newsletter?utm_campaign=csp&utm_source=nagioscore&utm_medium=newsletter&utm_content=<?php echo $this_version; ?>" target="_blank">Newsletter Sign-Up</a>
+			<a id="splashlearnmore" href="https://www.nagios.org/launch?utm_campaign=csp&utm_source=nagioscore&utm_medium=splash_button&utm_content=<?php echo $this_version; ?>" target="_blank">Learn More</a>
+			<a id="splashnewsletter" href="https://www.nagios.org/newsletter?utm_campaign=csp&utm_source=nagioscore&utm_medium=splash_newsletter_link&utm_content=<?php echo $this_version; ?>" target="_blank">Newsletter Sign-Up</a>
 		</div>
 	</div>
 </div>
